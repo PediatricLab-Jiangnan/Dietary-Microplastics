09071fefef65c3e227aa001fa9ec7598e8a8905b
PediatricLab-Jiangnan <zhaowenjingmo@163.com>
2024-11-08 08:06:02 +0800

    Update Correlation analysis.R

--- Correlation analysis.R
+++ Correlation analysis.R
@@ -9,7 +9,7 @@ library(corrplot)  # For creating correlation plots
 # ---------------------------
 
 # Use forward slashes to avoid escape character issues
-setwd("C:/Users/Ruiandxiaozhongzhong/Desktop/Microplastics/corr")
+setwd("C:/Users/RuiRui/Desktop/Microplastics/corr")
 
 # ---------------------------
 # 3. Load and Prepare the Data
